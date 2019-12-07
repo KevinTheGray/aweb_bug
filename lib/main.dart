ac7b443d2e3f835432d7ead831c08a121f865a36
Kevin Gray <KevinTheGray8@gmail.com>
2019-12-06 21:48:22 -0500

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Animated WEBP bug'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+            'assets/images/0-loop.webp',
+          ),
+          Image.asset(
+            'assets/images/1-loop.webp',
+          ),
+          Image.asset(
+            'assets/images/2-loop.webp',
+          ),
+        ],
+      ),
+    );
+  }
+}
